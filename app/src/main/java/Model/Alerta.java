ce45d86044a66e014f37febf845853d5fbe28861
Anderson Camacho <anddavidcz@gmail.com>
2026-02-28 22:19:46 -0500

    Completando las clases Alerta y CUltivo

--- app/src/main/java/Model/Alerta.java
+++ app/src/main/java/Model/Alerta.java
@@ -1,4 +1,41 @@
 package Model;
 
 public class Alerta {
+    private int alertaID;
+    private int zonaID;
+    private String tipoAlerta;
+    private String nivelSeveridad;
+    private String descripcion;
+    private String fechaEmision;
+    private String fechaInicio;
+    private String fechaFin;
+
+    public Alerta() {
+    }
+
+    public Alerta(int zonaID, String tipoAlerta, String nivelSeveridad, String descripcion, String fechaEmision, String fechaInicio, String fechaFin) {
+        this.zonaID = zonaID;
+        this.tipoAlerta = tipoAlerta;
+        this.nivelSeveridad = nivelSeveridad;
+        this.descripcion = descripcion;
+        this.fechaEmision = fechaEmision;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+
+    public int getAlertaID() { return alertaID; }
+
+    public int getZonaID() { return zonaID; }
+
+    public String getTipoAlerta() { return tipoAlerta; }
+
+    public String getNivelSeveridad() { return nivelSeveridad; }
+
+    public String getDescripcion() { return descripcion; }
+
+    public String getFechaEmision() { return fechaEmision; }
+
+    public String getFechaInicio() { return fechaInicio; }
+
+    public String getFechaFin() { return fechaFin; }
 }
--- app/src/main/java/Model/Cultivo.java
+++ app/src/main/java/Model/Cultivo.java
@@ -1,4 +1,63 @@
 package Model;
 
 public class Cultivo {
+    private int cultivoID;
+    private int zonaID;
+    private int usuarioID;
+    private int productoAgricolaID;
+    private String nombreCultivo;
+    private String descripcion;
+    private int areaCultivo;
+    private String fechaSiembra;
+    private String fechaCosechaEstimada;
+    private String fechaCosechaReal;
+    private String estadoCultivo;
+    private int tiempoRiegoPromedio;
+    private String fechaRegistro;
+
+    public Cultivo() {
+    }
+
+    public Cultivo(int zonaID, int usuarioID, int productoAgricolaID, String nombreCultivo, String descripcion, int areaCultivo, String fechaSiembra, String fechaCosechaEstimada, String fechaCosechaReal, String estadoCultivo, int tiempoRiegoPromedio, String fechaRegistro) {
+        this.zonaID = zonaID;
+        this.usuarioID = usuarioID;
+        this.productoAgricolaID = productoAgricolaID;
+        this.nombreCultivo = nombreCultivo;
+        this.descripcion = descripcion;
+        this.areaCultivo = areaCultivo;
+        this.fechaSiembra = fechaSiembra;
+        this.fechaCosechaEstimada = fechaCosechaEstimada;
+        this.fechaCosechaReal = fechaCosechaReal;
+        this.estadoCultivo = estadoCultivo;
+        this.tiempoRiegoPromedio = tiempoRiegoPromedio;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getCultivoID() { return cultivoID; }
+
+    public int getZonaID() { return zonaID; }
+
+    public void setZonaID(int zonaID) { this.zonaID = zonaID; }
+
+    public int getUsuarioID() { return usuarioID; }
+
+    public int getProductoAgricolaID() { return productoAgricolaID; }
+
+    public String getNombreCultivo() { return nombreCultivo; }
+
+    public String getDescripcion() { return descripcion; }
+
+    public int getAreaCultivo() { return areaCultivo; }
+
+    public String getFechaSiembra() { return fechaSiembra; }
+
+    public String getFechaCosechaEstimada() { return fechaCosechaEstimada; }
+
+    public String getFechaCosechaReal() { return fechaCosechaReal; }
+
+    public String getEstadoCultivo() { return estadoCultivo; }
+
+    public int getTiempoRiegoPromedio() { return tiempoRiegoPromedio; }
+
+    public String getFechaRegistro() { return fechaRegistro; }
 }
