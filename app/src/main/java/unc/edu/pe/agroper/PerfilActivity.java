19c0cc1a0c611bbce59a651ae0deebe5ad994238
YANERSON <yvasquezc23_2@unc.edu.pe>
2026-03-02 21:22:25 -0500

    Binding de la clase perfil

--- app/src/main/java/unc/edu/pe/agroper/PerfilActivity.java
+++ app/src/main/java/unc/edu/pe/agroper/PerfilActivity.java
@@ -8,13 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import unc.edu.pe.agroper.databinding.ActivityPerfilBinding;
+
 public class PerfilActivity extends AppCompatActivity {
 
+    ActivityPerfilBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_perfil);
+        binding = ActivityPerfilBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
